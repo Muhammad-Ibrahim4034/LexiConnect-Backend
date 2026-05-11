b56fc9f139e62d1f33b82622d4113a49039b00fc
Muhammad-Ibrahim4034 <mibrahim32666444@gmail.com>
2026-05-11 22:36:40 +0500

    link added

--- app/server.py
+++ app/server.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://lexi-connect-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
